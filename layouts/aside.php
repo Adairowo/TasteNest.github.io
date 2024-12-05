74815305ba3838677164ed2010d3306a7e8285e6
Adair <Adair.macias105@gmail.com>
2024-12-04 21:56:09 -0600

    Actualizacion
    Se agregaron las otras 4 pestañas de dashboard Usuarios, Menus, Pedidos

--- layouts/aside.php
+++ layouts/aside.php
@@ -11,33 +11,28 @@
     <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.3.1/css/hover-min.css">-->
         <link rel="stylesheet" href="css\hover.css">
 </head>
-<aside class="col-md-3 col-lg-2 d-md-block sidebar vh-100" style="background-color:rgb(10, 27, 44);">
+<aside class="col-md-3 col-lg-2 d-md-block sidebar vh-100 hvr-glow" style="background-color:rgb(10, 27, 44);">
      <div class="position-sticky">
         <h4 class="text-center text-light mb-5 py-3">TasteNest Dashboard</h4>
         <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link text-light mb-5 hvr-bob hvr-bounce-to-right w-100" href="./admin.html" style="background-color: #E98F3B;">
+                <a class="nav-link text-light mb-5 hvr-bob hvr-bounce-to-right w-100" href="./admin.php">
                     <i class="bi bi-house"></i> Inicio
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-light mb-5 hvr-bob hvr-bounce-to-right w-100" href="./users.html">
+                <a class="nav-link text-light mb-5 hvr-bob hvr-bounce-to-right w-100" href="./users.php">
                     <i class="bi bi-people"></i> Gestión de Usuarios
                 </a>
             </li>
                 <li class="nav-item">
-                  <a class="nav-link text-light mb-5 hvr-bob hvr-bounce-to-right w-100" href="./pedidos.html">
+                  <a class="nav-link text-light mb-5 hvr-bob hvr-bounce-to-right w-100" href="./orders.php">
                     <i class="bi bi-cart-dash"></i> Pedidos
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link text-light mb-5 hvr-bob hvr-bounce-to-right w-100" href="./services.html">
+                  <a class="nav-link text-light mb-5 hvr-bob hvr-bounce-to-right w-100" href="./menus.php">
                     <i class="bi bi-menu-app"></i>  Menus
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link text-light mb-5 hvr-bob hvr-bounce-to-right w-100" href="./stats.html">
-                    <i class="bi bi-bar-chart"></i> Estadisticas
                   </a>
                 </li>
               </ul>
