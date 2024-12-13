f0fc2432657e378ee289437e360e497f0ac96e37
Adair <Adair.macias105@gmail.com>
2024-12-12 21:32:26 -0600

    Actualizacion base de datos

--- tastenestdb.sql
+++ tastenestdb.sql
@@ -1,25 +1,17 @@
-CREATE DATABASE TasteNest;
+CREATE DATABASE dbtastenest;
 
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL, -- Asegúrate de almacenar la contraseña de forma segura (hash)
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    level ENUM('admin', 'user') DEFAULT 'user' -- Para definir el nivel de acceso
-);
-
-CREATE TABLE orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    status ENUM('pending', 'completed', 'canceled') DEFAULT 'pending',
-    total DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+    level ENUM('admin', 'user') DEFAULT 'user', -- Para definir el nivel de acceso
+    full_name  VARCHAR(30) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL -- Asegú
 );
 
 CREATE TABLE menu_items (
-id INT AUTO_INCREMENT PRIMARY KEY,
+menu_id INT AUTO_INCREMENT PRIMARY KEY,
 created_at DATETIME NOT NULL,
 imagen_menu VARCHAR(255) NOT NULL,
 titulo VARCHAR(20) NOT NULL,
@@ -31,4 +23,5 @@ CREATE TABLE Opiniones (
     opinion_id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(24) NOT NULL UNIQUE,
     opinion VARCHAR(255) NOT NULL
+    fecha_opinion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
